4e744ce95f318ea73517176c4629e31a498c73cb
hppRC <hpp.ricecake@gmail.com>
2019-06-25 09:53:34 +0900

    modify code

--- tact/tact.ino
+++ tact/tact.ino
@@ -1,18 +1,27 @@
 #include "enpitshield.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  //タクトスイッチ1と全てのLEDの初期設定を行う
   pinMode(TACTSW0, INPUT);
   for (int j=0; j<4; j++) {
     pinMode(LED_list[j], OUTPUT); 
   }
 }
 
+
+//LEDを先に進める用の変数
 int i = 0;
+
+//LEDを変更するのに必要なボタン押下の回数
 int RECOG_RATE = 1;
+
+//スイッチの押下継続を判定するためのフラグ
 int push_continue_flag = false;
 
+
 void loop() {
+  //スイッチを押し続けても点灯LEDが遷移しないように条件分岐させる
+  //一度押下していない状態を挟むと、再度入力ができるようになる
   if ((digitalRead(TACTSW0) == TACTSW_ON) && (!push_continue_flag)) {
     i++;
     i %= RECOG_RATE*4;
@@ -20,14 +29,15 @@ void loop() {
   } else if ((digitalRead(TACTSW0) == TACTSW_OFF) || (!push_continue_flag)) {
     push_continue_flag = false;
   } 
-  
+
+  //点灯するLEDを決定
   int led = i/RECOG_RATE;
-  if ((led) != 0) {
+  
+  if (led != 0) {
     digitalWrite(LED_list[led-1], LED_OFF);
-    digitalWrite(LED_list[led], LED_ON);
   } else {
     digitalWrite(LED_list[3-led], LED_OFF);
-    digitalWrite(LED_list[led], LED_ON);
   }
+  digitalWrite(LED_list[led], LED_ON);
   return;
 }
